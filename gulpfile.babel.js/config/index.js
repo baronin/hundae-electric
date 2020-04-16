1d30f28e3ec67a26b245b5485b50b6ea2ad19c85
Yaroslav <baronin.yaroslav@gmail.com>
2020-04-16 16:18:35 +0300

    SÐThe work environment has been configured

---
+++ gulpfile.babel.js/config/index.js
@@ -0,0 +1,44 @@
+const paths = {
+  src: "./src",
+  dist: "./dist",
+
+  nunjucks: {
+    /* ['pages/index/index.html', 'pages/index/about.html'] - multiple entry points */
+    src: ["pages/main/main.njk"],
+    dist: "",
+  },
+
+  stylesheets: {
+    /* ['pages/index/index.scss', 'pages/index/about.scss'] - multiple entry points */
+    src: "index.scss",
+    dist: "assets/css",
+  },
+
+  javascripts: {
+    /* ['pages/index/index.js', 'pages/index/about.js'] - multiple entry points */
+    src: "index.js",
+    dist: "assets/js",
+  },
+
+  images: {
+    src: "images/**/*",
+    dist: "assets/img",
+  },
+
+  fonts: {
+    src: "fonts/**/*",
+    dist: "assets/fonts",
+  },
+};
+
+const ftps = {
+  host: "host",
+  user: "user",
+  password: "password",
+  folder: "folder",
+};
+
+module.exports = {
+  paths,
+  ftps,
+};
