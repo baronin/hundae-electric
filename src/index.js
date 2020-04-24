46a74111ad494f92ea662a0f4b0c2251bb29abcc
Yaroslav <baronin.yaroslav@gmail.com>
2020-04-25 02:54:38 +0300

    Include slick

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 /*  example require("./pages/main/main"); */
 window.$ = window.jQuery = require('jquery');
+window.slick = require('slick-carousel');
+
 // Plugins
 require('jquery-mask-plugin');
 require('jquery-validation');
 require('plugins/validation/additional-method.min');
+require('plugins/slick/slick');
 
 require("common/js/mask");
 require("common/js/validation");
